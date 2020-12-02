ec899f3ec0b5f437f2f133f99abdced5e8203243
liushuo <364725259@qq.com>
2020-12-02 12:20:00 +0800

    引入tcbrouter扩展云函数

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -1,12 +1,20 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+const tcbRouter = require('tcb-router');
+const TcbRouter = require('tcb-router');
 
 cloud.init()
 const db = cloud.database();
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return await db.collection('playlist').skip(event.start).limit(event.count).orderBy('createTime','desc').get().then((res)=>{
-    return res;
-  })
+  const app = new TcbRouter({event});
+  app.router('playlist',async(ctx, next) => {
+    ctx.body =  await db.collection('playlist').skip(event.start)
+                .limit(event.count).orderBy('createTime','desc').get().then((res)=>{
+                    return res;
+                })
+  });
+
+  return app.serve();
 }
