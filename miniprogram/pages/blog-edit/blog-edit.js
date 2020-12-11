e54cae317d40964bba5787f0858de39f3f600091
liushuo <364725259@qq.com>
2020-12-11 10:07:35 +0800

    博客插入数据库

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,7 +1,11 @@
 // pages/blog-edit/blog-edit.js
 const MAX_WORDS = 140;
 const MAX_IMGS = 9;
-
+const promises = [];
+const db = wx.cloud.database();
+let userInfo = {};
+let content= "";
+let fileIds = []
 Page({
 
   /**
@@ -10,15 +14,16 @@ Page({
   data: {
     wordNum: 0,
     selectedImages: [],
-    showSelect: true
+    showSelect: true,
   },
 
   textInput(event) {
-    console.log(event)
+    //console.log(event)
     this.setData({
       wordNum: event.detail.value.length,
-      footerBottom: 0
-    })
+      footerBottom: 0,
+    });
+    content = event.detail.value;
   },
 
   onFocus(event) {
@@ -37,8 +42,8 @@ Page({
     let max = MAX_IMGS - this.data.selectedImages.length;
     wx.chooseImage({
       count: max,
-      sizeType: ["original","compressed"],
-      sourceType: ['album','camera'],
+      sizeType: ["original", "compressed"],
+      sourceType: ['album', 'camera'],
       success: (res) => {
         console.log(res);
         this.setData({
@@ -46,7 +51,7 @@ Page({
         })
         max = MAX_IMGS - this.data.selectedImages.length;
         this.setData({
-          showSelect: max <= 0? false: true
+          showSelect: max <= 0 ? false : true
         })
       }
     })
@@ -54,12 +59,12 @@ Page({
 
   removeImage(event) {
     console.log(event.target.dataset.index);
-    this.data.selectedImages.splice(event.target.dataset.index,1)
+    this.data.selectedImages.splice(event.target.dataset.index, 1)
     this.setData({
       selectedImages: this.data.selectedImages
     })
 
-    if (this.data.selectedImages.length == MAX_IMGS-1) {
+    if (this.data.selectedImages.length == MAX_IMGS - 1) {
       this.setData({
         showSelect: true
       })
@@ -74,22 +79,61 @@ Page({
   },
 
   send() {
-    //上传到云存储
-    for (let i=0;i<this.data.selectedImages.length;i++) {
-      let item = this.data.selectedImages[i];
-      let suffix = /\.\w+$/.exec(item); //获取扩展名
-      console.log(suffix);
-      wx.cloud.uploadFile({
-        cloudPath: "blog/" + Date.now + '-' + Math.random() * 1000000 + suffix,
-        filePath: item,
-        success: (res) => {
-
-        },
-        fail: (res) => {
-          console.log(res);
-        }
+    if (content.trim().length == "") {
+      wx.showModal({
+        title: '发布内容不能为空',
       })
+      return;
     }
+    wx.showLoading({
+      title: '发布中...',
+    })
+    for (let i = 0; i < this.data.selectedImages.length; i++) {
+      let _promise = new Promise((resole, reject) => {
+        let item = this.data.selectedImages[i];
+        let suffix = /\.\w+$/.exec(item); //获取扩展名
+        console.log(suffix);
+        wx.cloud.uploadFile({ //上传到云存储
+          cloudPath: "blog/" + Date.now() + '-' + Math.random() * 1000000 + suffix,
+          filePath: item,
+          success: (res) => {
+            fileIds = fileIds.concat(res.fileID);
+            resole(res);
+          },
+          fail: (res) => {
+            console.log(res);
+            reject(res);
+          }
+        })
+      })
+      promises.push(_promise)
+    };
+    Promise.all(promises).then((res) => {
+      //调用云函数插入数据库
+      db.collection('blog').add({
+        data: {
+          ...userInfo,
+          createTime: db.serverDate(),
+          content,
+          images: fileIds
+        }
+      }).then((res)=> {
+        wx.hideLoading({
+          success: (res) => {
+            wx.showToast({
+              title: '发布成功',
+            })
+          },
+        });
+        wx.navigateBack();
+      });
+    }).catch((exception) => {
+      console.log(exception);
+      wx.hideLoading();
+      wx.showToast({
+        title: '发布失败',
+      })
+    });
   },
 
   /**
@@ -97,6 +141,8 @@ Page({
    */
   onLoad: function (options) {
     console.log(options);
+    userInfo.nickName = options.nickName;
+    userInfo.avatar = options.avatarUrl;
   },
 
   /**
