d79e2340e9e470dc753c7d6dcc908613c45fbd56
liushuo <364725259@qq.com>
2020-12-14 19:16:22 +0800

    添加评论到数据库

--- miniprogram/components/blog-ctrl/blog-ctrl.js
+++ miniprogram/components/blog-ctrl/blog-ctrl.js
@@ -1,11 +1,12 @@
 // components/blog-ctrl/blog-ctrl.js
 let userInfo = {}
+let db = wx.cloud.database()
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    blogId: String
   },
 
   externalClasses: ['iconfont','icon-pinglun','icon-fenxiang'],
@@ -44,19 +45,58 @@ Component({
      })
     },
 
-    onLoginSuccess() {
+    onLoginSuccess(event) {
       this.setData({
         loginShow: false
       }, () => {
         this.setData({
           modalShow : true
         })
+        userInfo = event.detail
+        console.log(userInfo)
       })
     },
 
     onLoginFail() {
       wx.showModal({
         content: "授权用户才能访问",
+      })
+    },
+
+    onSend(event) {
+      if(this.data.content.trim() == '') {
+        wx.showModal({
+          content: '评论不能为空',
+        })
+        return
+      }
+      //console.log(this.data.content)
+      //存储数据
+      wx.showLoading({
+        title: '评价中',
+      })
+
+      db.collection("blog-comment").add({
+        data : {
+          content:this.data.content,
+          createTime: db.serverDate(),
+          blogId: this.properties.blogId,
+          nickName: userInfo.nickName,
+          avatar: userInfo.avatarUrl
+        }
+      }).then( (res) => {
+        //console.log("插入成功")
+        wx.hideLoading()
+        this.setData({
+          modalShow: false,
+          content:''
+        })
+      })
+    },
+
+    onInput(event) {
+      this.setData({
+        content: event.detail.value
       })
     }
   }
