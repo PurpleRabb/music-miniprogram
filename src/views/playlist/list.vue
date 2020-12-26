c2c80f96d2302410173872d24ad09def6c3350f9
liushuo <364725259@qq.com>
2020-12-26 17:23:21 +0800

    前台获取歌单列表

--- src/views/playlist/list.vue
+++ src/views/playlist/list.vue
@@ -1,15 +1,60 @@
 <template>
-    <div>
-        歌单
-    </div>
+  <div>
+    <el-table v-loading="loading" :data="playlist" stripe>
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column label="封面" width="100">
+        <template slot-scope="scope">
+          <img :src="scope.row.picUrl" alt height="50" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="copywriter" label="描述"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="onEdit(scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="onDel(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import { fetchList } from "@/api/playlist";
+import scroll from "@/utils/scroll"
+
 export default {
-    
-}
+  data() {
+    return {
+      playlist: [],
+      count: 50,
+      loading: false
+    };
+  },
+  created() {
+    this.getList();
+  },
+  mounted() {
+      scroll.start(this.getList)
+  },
+  methods: {
+    getList() {
+      this.loading = true
+      fetchList({
+        start: this.playlist.length,
+        count: this.count,
+      }).then((res) => {
+        console.log(res);
+        this.playlist = this.playlist.concat(res.data)
+        if (res.data.length < this.count) {
+            scroll.end()
+        }
+        this.loading = false
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
